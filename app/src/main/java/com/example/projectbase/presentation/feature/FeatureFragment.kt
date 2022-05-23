daf458fe9b8e82c6f3ed9e6adbb7c5d94008b3e7
taisa <taisazevedo@hotmail.com>
2022-05-23 11:05:02 -0300

    Update fragment DI.

--- app/src/main/java/com/example/projectbase/presentation/feature/FeatureFragment.kt
+++ app/src/main/java/com/example/projectbase/presentation/feature/FeatureFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.projectbase.databinding.FragmentFeatureBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FeatureFragment : Fragment() {
 
     private var _binding: FragmentFeatureBinding? = null
